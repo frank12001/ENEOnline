9f03002daf52efe93081f9388261cc0da3ca286e
FanTang <gihhgihh11@gmail.com>
2017-07-01 01:25:37 +0800

    Add files via upload

---
+++ gaming/Baseplayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 基本的玩家單位。(其實就只是存玩家的屬性，隨時可新增)
+/// </summary>
+public class Baseplayer
+{
+    /// <summary>
+    /// 給予值
+    /// </summary>
+    /// <param name="uid">玩家的uid</param>
+    /// <param name="name">玩家遊戲名子</param>
+    public Baseplayer(int uid, string name)
+    {
+        this.uid = uid;
+        this.name = name;
+        this.heroId = -1;
+    }
+    public int uid;
+    public string name;
+    //玩家在房間中的第幾個
+    public byte playId;
+    //是在哪一隊
+    public byte teamNumber;
+    public int heroId;
+}
